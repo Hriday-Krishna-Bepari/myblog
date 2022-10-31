b6c1baece49b56d906fdaa0dd18da2075491dd1a
Hriday Krishna Bepari <60868126+Hriday-Krishna-Bepari@users.noreply.github.com>
2022-10-31 14:24:31 +0600

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "epiz_32898269");
+define("DB_PASS", "zTWTFhjTYh7M9");
+define("DB_NAME", "epiz_32898269_XXX");
+define("TITLE", "Tourist Spot");
+define("KEYWORDS", "Tour");
